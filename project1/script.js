146cf2eb82044df8794c225d24a7ade51a2dbc02
calebzroberts <caleb.roberts798@gmail.com>
2025-10-22 10:35:31 -0400

    Added add item function

---
+++ project1/script.js
@@ -0,0 +1,24 @@
+//create grocery list
+//add items adds items to list
+//when item added to list, update on screen list
+//onclick item either removes or unremoves based on removal status
+
+let groceryList = [];
+const onScreenList = document.getElementById("groceryList");
+
+function AddItem(name)
+{
+    groceryList.push(name);
+    UpdateOnScreenList();
+}
+
+function UpdateOnScreenList()
+{
+    //add each item to the visible list
+    for (let i = 0; i < groceryList.length; i++)
+    {
+        let newVisibleItem = document.createElement("p");
+        newVisibleItem.innerText = groceryList[i];
+        onScreenList.append(newVisibleItem);
+    }
+}
