2e24d29dea927072a0eecec59aa5fa9e1889920d
calebzroberts <caleb.roberts798@gmail.com>
2025-10-23 08:15:26 -0400

    added screen change logic

--- project1/script.js
+++ project1/script.js
@@ -5,6 +5,12 @@
 
 
 let groceryList = [];
+let purchasedItems = [];
+let unpurchasedItems = [];
+
+//displaying list shows either fullList (default), purchasedItems, or unpurchasedItems;
+let displayingList = "fullList";
+
 const onScreenList = document.getElementById("groceryList");
 
 function AddButtonClicked()
@@ -12,13 +18,18 @@ function AddButtonClicked()
     let userItem = prompt(`What do you want to add?`);
     AddItem(userItem, false);
 }
-function AddItem(name, removed)
+function AddItem(name, purchased)
 {
     const item = {
         name: name,
-        removed: removed
+        purchased: purchased
     };
+
+    //add item by default to both unpurchased and full list
     groceryList.push(item);
+    unpurchasedItems.push(item);
+
+    //update the visible list
     UpdateOnScreenList();
 }
 
@@ -27,41 +38,111 @@ function UpdateOnScreenList()
     //reset on screen list
     onScreenList.innerHTML = "";
 
-    //add each item to the visible list
-    for (let i = 0; i < groceryList.length; i++)
+    //will update list with appropriate list
+    if (displayingList === "fullList")
     {
-        let newVisibleItem = document.createElement("div");
-
-        //if item crossed out, cross it out
-        if (groceryList[i].removed === true)
+        //add each item to the visible list
+        for (let i = 0; i < groceryList.length; i++)
         {
-            newVisibleItem.innerHTML = `
-            <p value = "${i}" style="cursor:pointer" onclick="ToggleRemoval(${i})"><s>${groceryList[i].name}</s></p>
-            `;
-            onScreenList.append(newVisibleItem);
-        }
-        else if (groceryList[i].removed === false)
-        {
-            newVisibleItem.innerHTML = `
-            <p value = "${i}" style="cursor:pointer" onclick="ToggleRemoval(${i})">${groceryList[i].name}</p>
-            `;
-            onScreenList.append(newVisibleItem);
-        }
+            let newVisibleItem = document.createElement("div");
 
-        
+            //if item crossed out, cross it out
+            if (groceryList[i].purchased === true)
+            {
+                newVisibleItem.innerHTML = `
+                <p value = "${i}" style="cursor:pointer" onclick="ToggleRemoval(${i})"><s>${groceryList[i].name}</s></p>
+                `;
+                onScreenList.append(newVisibleItem);
+            }
+            else if (groceryList[i].purchased === false)
+            {
+                newVisibleItem.innerHTML = `
+                <p value = "${i}" style="cursor:pointer" onclick="ToggleRemoval(${i})">${groceryList[i].name}</p>
+                `;
+                onScreenList.append(newVisibleItem);
+            }
+        }
     }
+
+    //will update list with appropriate list
+    else if (displayingList === "purchasedList")
+    {
+        //add each item to the visible list
+        for (let i = 0; i < purchasedItems.length; i++)
+        {
+            let newVisibleItem = document.createElement("div");
+
+            //if item crossed out, cross it out
+            if (purchasedItems[i].purchased === true)
+            {
+                newVisibleItem.innerHTML = `
+                <p value = "${i}" style="cursor:pointer" onclick="ToggleRemoval(${i})"><s>${purchasedItems[i].name}</s></p>
+                `;
+                onScreenList.append(newVisibleItem);
+            }
+            else if (purchasedItems[i].purchased === false)
+            {
+                newVisibleItem.innerHTML = `
+                <p value = "${i}" style="cursor:pointer" onclick="ToggleRemoval(${i})">${purchasedItems[i].name}</p>
+                `;
+                onScreenList.append(newVisibleItem);
+            }
+        }
+    }
+
+    //will update list with appropriate list
+    else if (displayingList === "unpurchasedList")
+    {
+        //add each item to the visible list
+        for (let i = 0; i < unpurchasedItems.length; i++)
+        {
+            let newVisibleItem = document.createElement("div");
+
+            //if item crossed out, cross it out
+            if (unpurchasedItems[i].purchased === true)
+            {
+                newVisibleItem.innerHTML = `
+                <p value = "${i}" style="cursor:pointer" onclick="ToggleRemoval(${i})"><s>${unpurchasedItems[i].name}</s></p>
+                `;
+                onScreenList.append(newVisibleItem);
+            }
+            else if (unpurchasedItems[i].purchased === false)
+            {
+                newVisibleItem.innerHTML = `
+                <p value = "${i}" style="cursor:pointer" onclick="ToggleRemoval(${i})">${unpurchasedItems[i].name}</p>
+                `;
+                onScreenList.append(newVisibleItem);
+            }
+        }
+    }
+    
 }
 
 function ToggleRemoval(index)
 {
-    if (groceryList[index].removed === false)
+    //if adding item, adjust lists accordingly
+    if (groceryList[index].purchased === false)
     {
-        groceryList[index].removed = true;
+        groceryList[index].purchased = true;
+        unpurchasedItems.splice(index, 1);
+        purchasedItems.push(groceryList[index]);
         UpdateOnScreenList();
     }
-    else if (groceryList[index].removed === true)
+    //if removing purchased status, then adjust lists accordingly
+    else if (groceryList[index].purchased === true)
     {
-        groceryList[index].removed = false;
-        UpdateOnScreenList();
+        groceryList[index].purchased = false;
+        unpurchasedItems.push(groceryList[i]);
+        purchasedItems.splice(index, 1);
+        UpdateOnScreenList();   
     }
+}
+
+function ChangeItemView(value)
+{
+    //if full list, then display full list
+    //else if purchased items, then display purchased items
+    //else if unpurchased items, then display unpurchased items
+    displayingList = value;
+    UpdateOnScreenList();
 }
